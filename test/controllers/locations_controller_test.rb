03c5c95f48134e3f3d88d588bbf4cf1f80945272
John Ivanoff <john.ivanoff@gmail.com>
2024-02-29 23:43:52 -0500

    test(locations_controller_test): added postal code to params
    also set the post as turbo stream

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class LocationsControllerTest < ActionDispatch::IntegrationTest
   test "should get update" do
-    params = { "location"=>{ "latitude"=>52.88352131012385, "longitude"=>-118.14365295246522, "countryCode"=>"us" } }
-    post update_location_url(params)
+    params = { "location"=>{ "latitude"=>52.88352131012385, "longitude"=>-118.14365295246522, "countryCode"=>"us", "postCode" => "90210" } }
+    post update_location_url(params), as: :turbo_stream
     assert_response :success
   end
 end
